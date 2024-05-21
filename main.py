0c2c0224550072ae3ea3597d718d2d5da9767acb
Agustín Alexander Turchetti Sahajdachy <69662336+agustur@users.noreply.github.com>
2024-05-21 10:33:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+@app.get("/", response_class=Response)
 def amor_de_mi_vida():
-    LM = Luisana Morales te amoooooooooooooooo<3
-    return LM
+    LM = "Luisana Morales te amoooooooooooooooo<3"
+    return Response(LM, media_type="text/plain")
